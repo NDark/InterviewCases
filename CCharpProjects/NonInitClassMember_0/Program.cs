b55e485c63fe865c3f8cae62859dfe33a1c38911
NDark <ndarkteng@gmail.com>
2016-03-20 02:06:47 +0800

    add project NonInitClassMember_0

---
+++ CCharpProjects/NonInitClassMember_0/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonInitClassMember_0
+{
+    class Tooth
+    {
+        public int Age { get; set; }
+    }
+
+    class Character
+    {
+    	public void Print()
+        {
+            System.Console.Out.WriteLine( m_Tooth0.Age);
+            System.Console.Out.WriteLine(m_Tooth1.Age);
+        }
+
+	    private Tooth m_Tooth0 { get; set; }
+        private Tooth m_Tooth1;
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character obj = new Character();
+            obj.Print();
+
+            System.Console.In.ReadLine();
+        }
+    }
+}
