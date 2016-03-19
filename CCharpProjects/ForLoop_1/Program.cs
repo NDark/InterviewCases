437a8ccc2164d4c1a05a8f3b01aafd51172055f8
NDark <ndarkteng@gmail.com>
2016-03-20 02:01:43 +0800

    add project ForLoop_1

---
+++ CCharpProjects/ForLoop_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> resourceVec = new List<int>();
+
+            int loopNum = 100;
+            int count = 0;
+
+            for (; loopNum >= 0; --loopNum)
+            {
+                resourceVec.Add(0);
+                System.Console.Out.WriteLine(++count);
+            }
+
+            
+            System.Console.In.ReadLine();
+        }
+    }
+}
