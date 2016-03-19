7d2ee3b35c4adb184a5068ff2ab31a641726eff2
NDark <ndarkteng@gmail.com>
2016-03-19 14:56:01 +0800

    remove calculation

--- UnityProjects/UnityProject_0/Assets/GameObjectUpdate/TryMoveTo.cs
+++ UnityProjects/UnityProject_0/Assets/GameObjectUpdate/TryMoveTo.cs
@@ -8,22 +8,25 @@ using System.Collections.Generic;
 
 public class TryMoveTo : MonoBehaviour 
 {
-	static List<TryMoveTo> s_List = new List<TryMoveTo>() ;
-	
 	const float BOX_SIZE = 5.0f ;
 	Vector3 m_Goal = Vector3.zero;
 
 	// Use this for initialization
 	void Start () 
 	{
-		s_List.Add( this ) ;
-	
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if( Vector3.Distance( m_Goal , this.transform.position ) < 1.0f )
+		DoUpdate()
+	}
+	
+	public void DoUpdate()
+	{
+		GameObject thisObject = this.gameObject ;
+		
+		if( Vector3.Distance( m_Goal , thisObject.transform.position ) < 1.0f )
 		{
 			Vector3 newPos = Random.insideUnitSphere ;
 			newPos *= BOX_SIZE ;
@@ -31,21 +34,12 @@ public class TryMoveTo : MonoBehaviour
 		}
 		else
 		{
-			CalculateDistanceBetween();
 			
-			this.transform.position = 
-			Vector3.Lerp( this.transform.position , m_Goal , Time.deltaTime ) ;
+			thisObject.transform.position = 
+				Vector3.Lerp( thisObject.transform.position 
+				             , m_Goal , Time.deltaTime ) ;
 			
 			
-		}
-	}
-	
-	private void CalculateDistanceBetween()	
-	{
-		float sum = 0.0f ;
-		for( int i = 0 ; i < 10000 ; ++i )
-		{
-			sum += Mathf.Sqrt( i * i / 2.0f ) ;
 		}
 	}
 }
