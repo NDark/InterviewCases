63891d501502aaca8f9fa2d18c4d07f2bc38b76b
NDark <ndarkteng@gmail.com>
2016-03-19 14:11:05 +0800

    add Scene UnityProject_0

---
+++ UnityProjects/UnityProject_0/Assets/ResourcesLoad/ResourcesLoad.cs
@@ -0,0 +1,23 @@
+﻿/**
+
+*/
+using UnityEngine;
+
+public class ResourcesLoad : MonoBehaviour 
+{
+	public Material m_ObjMaterial = null ;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		Texture2D tex2D = Resources.Load("WhileSliced") as Texture2D;
+		
+		m_ObjMaterial.mainTexture = tex2D ;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
