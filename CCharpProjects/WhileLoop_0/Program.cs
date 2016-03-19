7c9384a0276303c1ad41c51130d7e966c4ddc6d6
NDark <ndarkteng@gmail.com>
2016-03-20 02:33:06 +0800

    add project WhileLoop_0

---
+++ CCharpProjects/WhileLoop_0/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop_0
+{
+
+ 
+
+
+    class Program
+    {
+        static Random sRandom = null;
+
+        static int GetResource()
+        {
+            int ret = (0 == sRandom.Next( 2 ) ) ? 1 : 2;
+            return ret;
+        }
+
+        static void Main(string[] args)
+        {
+            sRandom = new Random();
+            int goal = 100;
+            int resource = 0;
+            while (true)
+            {
+                resource += GetResource();
+                Console.WriteLine(resource);
+                if (resource == goal)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(resource);
+
+            Console.ReadLine();
+        }
+    }
+}
