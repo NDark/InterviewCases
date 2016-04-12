aed6f32e56b1eb3978ac2317010f99875f8c4509
NDark <ndarkteng@gmail.com>
2016-04-12 20:58:12 +0800

    add Destroy.meta

---
+++ UnityProjects/UnityProject_0/Assets/Destroy/MoveVec.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveVec : MonoBehaviour 
+{
+	public Vector3 m_Vec = Vector3.zero ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.Translate( m_Vec ) ;
+	}
+}
---
+++ UnityProjects/UnityProject_0/Assets/Destroy/OnTriggerEnterCode.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnTriggerEnterCode : MonoBehaviour {
+
+	
+	void OnTriggerEnter( Collider _Other )
+	{
+		Debug.Log("OnTriggerEnter" + _Other.name );
+		// die here.
+		GameObject.Destroy( this ) ;
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
