8f8785fa10a152887bc5357d44e1301283a1da83
NDark <ndarkteng@gmail.com>
2016-04-12 20:50:08 +0800

    add OnCollisionEnterCode.cs

---
+++ UnityProjects/UnityProject_0/Assets/Collision_R/OnCollisionEnterCode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnCollisionEnterCode : MonoBehaviour 
+{
+	void OnCollisionEnter(Collision _Collision )
+	{
+		Debug.Log("OnCollisionEnter " + _Collision.gameObject.name );
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
