18aa0d383ba23d67ae71d48c1cae8c60251872c0
NDark <ndarkteng@gmail.com>
2016-05-04 20:24:06 +0800

    add null checking

--- CCharpProjects/WhileLoop_0/Program.cs
+++ CCharpProjects/WhileLoop_0/Program.cs
@@ -6,25 +6,27 @@ using System.Threading.Tasks;
 
 namespace WhileLoop_0
 {
-
- 
-
-
     class Program
     {
         static Random sRandom = null;
 
         static int GetResource()
         {
-            int ret = (0 == sRandom.Next( 2 ) ) ? 1 : 2;
+            int ret = 0 ;
+            if( null != sRandom )
+            {
+                ret = (0 == sRandom.Next(2)) ? 1 : 2;
+            }
             return ret;
         }
 
         static void Main(string[] args)
         {
             sRandom = new Random();
+
             int goal = 100;
             int resource = 0;
+
             while (true)
             {
                 resource += GetResource();
