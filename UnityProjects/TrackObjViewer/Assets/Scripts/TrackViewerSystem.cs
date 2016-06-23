68d9b4858b93571a2e094e96c2d3ecdc723736db
NDark <ndarkteng@gmail.com>
2016-06-23 19:34:57 +0800

    add project TrackObjViewer

---
+++ UnityProjects/TrackObjViewer/Assets/Scripts/TrackViewerSystem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackViewerSystem : MonoBehaviour 
+{
+
+	public GameObject m_ObjectParent = null ;
+	public Camera m_Camera = null ;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
